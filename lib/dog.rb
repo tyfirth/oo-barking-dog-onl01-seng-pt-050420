429902d1a11562ecf864c743c11b1f426e644e2e
Tyler Firth <tyfirth@gmail.com>
2020-05-19 01:39:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Dog 
+  
+  def name= (doggy)
+    @dogs_name = doggy
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
+
+rex = Dog.new 
+
+rex.name = "Rex"
+
+rex.name
+
+rex.bark
